1f3b5aad75a87e2b3655d1a75e11780747e8264c
Nick Peto <nick.petosa@gmail.com>
2016-01-31 22:26:59 -0500

    AntLabMain P5

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,10 +71,10 @@ import edu.gatech.oad.antlab.pkg2.*;
           //and gburdell4 with your gt id
           Person4 p4 = new Person4("William Broome");
           toPrint += p4.toString("wbroome3");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("Nicholas Petosa");
+          toPrint += p5.toString("npetosa3");
 
         System.out.println(toPrint);
 
