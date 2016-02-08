eac1f5b153993ede512a63cd29a9cd7d5d6fb098
Nick Peto <nick.petosa@gmail.com>
2016-02-08 04:22:23 -0500

    Added M3

---
+++ M3/MovieRecommender/app/src/main/java/edu/gatech/movierecommender/LoginActivity.java
@@ -0,0 +1,34 @@
+package edu.gatech.movierecommender;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void buttonOnClick(View v) {
+        EditText usernameBox = (EditText) findViewById(R.id.usernameTextBox);
+        EditText passwordBox = (EditText) findViewById(R.id.passwordTextBox);
+        String username = usernameBox.getText().toString();
+        String password = passwordBox.getText().toString();
+
+        if (username.equals("user") && password.equals("pass")) {
+            Intent dashboardIntent = new Intent(getApplicationContext(), Dashboard.class);
+            startActivity(dashboardIntent);
+        } else {
+            Toast.makeText(this, "Incorrect username or password.", Toast.LENGTH_LONG).show();
+            usernameBox.setText("");
+            passwordBox.setText("");
+        }
+    }
+
+}
