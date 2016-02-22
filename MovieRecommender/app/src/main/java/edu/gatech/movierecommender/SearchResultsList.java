8006abd31510641992aee063089dbf6eef29ee70
Davey Smith <daveysmith14@gmail.com>
2016-02-22 18:46:13 -0500

    Update to include api stuff

--- MovieRecommender/app/src/main/java/edu/gatech/movierecommender/SearchResultsList.java
+++ MovieRecommender/app/src/main/java/edu/gatech/movierecommender/SearchResultsList.java
@@ -12,5 +12,18 @@ public class SearchResultsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchresults);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        String query = intent.getStringExtra("QUERY");
+        if(query.equals("New Releases")) {
+            // Do nothing right now
+        }else if(query.equals("New to DVD")) {
+            // Do nothing right now
+        } else {
+            encodedQuery = query.replaceAll(" ", "+");
+            String url = "http://www.omdbapi.com/?s=" + encodedQuery;
+        }
     }
 }
