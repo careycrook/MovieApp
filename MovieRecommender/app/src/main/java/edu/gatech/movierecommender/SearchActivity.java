5af8db6421e6da0d39903afca45f76d353e804c9
William Ma <willzma15@gmail.com>
2016-03-06 22:41:18 -0500

    sorted ratings etc. 2

--- MovieRecommender/app/src/main/java/edu/gatech/movierecommender/SearchActivity.java
+++ MovieRecommender/app/src/main/java/edu/gatech/movierecommender/SearchActivity.java
@@ -26,6 +26,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 
 public class SearchActivity extends AppCompatActivity {
@@ -104,8 +105,6 @@ public class SearchActivity extends AppCompatActivity {
             mRequestQueue.add(stringRequest);
 
         } else if (query.equals("TOP")){
-            System.out.println("gay");
-
             ArrayList<Movie> movieList = new ArrayList<Movie>(World.videoHash.values());
             Collections.sort(movieList);
 
@@ -113,11 +112,33 @@ public class SearchActivity extends AppCompatActivity {
                 System.out.println(m);
             }
         } else {
+            ArrayList<Movie> movieList = new ArrayList<Movie>(World.videoHash.values());
+            ArrayList<Movie> majorMovieList = new ArrayList<Movie>();
+            String major = World.currentUser.getProfile().getMajor();
 
+            for (Movie m : movieList) {
+                if (m.getMajorRatings().containsKey(major)) {
+                    majorMovieList.add(m);
+                }
+            }
 
-            ///
+            Collections.sort(majorMovieList, new Comparator<Movie>() {
+                public int compare(Movie m1, Movie m2) {
+                    String compMajor = World.currentUser.getProfile().getMajor();
 
+                    if (m1.getAverageMajorRating(compMajor) > m2.getAverageMajorRating(compMajor)) {
+                        return -1;
+                    } else if (m1.getAverageMajorRating(compMajor) == m2.getAverageMajorRating(compMajor)) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                }
+            });
 
+            for (Movie m : majorMovieList) {
+                System.out.println(m);
+            }
         }
     }
 }
