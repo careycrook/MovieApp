34e4b1e148185178d2e3da8b4ca05d3f0c00395e
William Ma <willzma15@gmail.com>
2016-04-03 05:14:55 -0400

    Added Android JUnit4

--- MovieRecommender/app/src/androidTest/java/edu/gatech/movierecommender/DBTests.java
+++ MovieRecommender/app/src/androidTest/java/edu/gatech/movierecommender/DBTests.java
@@ -1,6 +1,7 @@
 package edu.gatech.movierecommender;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteException;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -59,6 +60,28 @@ public class DBTests extends AndroidTestCase {
                 "imgURL TEXT NOT NULL)");
 
         Movie m2 = DBHelper.getMovie("Up");
+
+        assertTrue(m2 == null);
+    }
+
+    @Test(expected = SQLiteException.class)
+    public void getMovieInvalidSQL() {
+        this.getContext().deleteDatabase("testDB1.db");
+
+        World.setDatabase(this.getContext().openOrCreateDatabase("testDB1.db", Context.MODE_PRIVATE, null));
+
+        World.getDatabase().execSQL("DROP TABLE IF EXISTS movies");
+
+        World.getDatabase().execSQL("CREATE TABLE IF NOT EXISTS movies (_id INTEGER PRIMARY KEY "
+                + "AUTOINCREMENT, title TEXT NOT NULL, averageRating REAL NOT NULL DEFAULT '0', " +
+                "imgURL TEXT NOT NULL)");
+
+        Movie m = new Movie("The Hunger Games");
+        m.setUrl("URL");
+        m.setAverageRating(5);
+        DBHelper.addNewMovie(m);
+
+        Movie m2 = DBHelper.getMovie(";\'");
 
         assertTrue(m2 == null);
     }
