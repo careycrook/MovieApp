aa6f6b12a1c9d2ae717bfa24a6008027f250ab5d
William Ma <willzma15@gmail.com>
2016-04-03 03:44:08 -0400

    Added Android JUnit4

---
+++ MovieRecommender/app/src/androidTest/java/edu/gatech/movierecommender/DBTests.java
@@ -0,0 +1,15 @@
+package edu.gatech.movierecommender;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by theon on 4/3/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class DBTests {
+
+}
